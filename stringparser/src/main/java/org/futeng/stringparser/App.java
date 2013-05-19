af6700b6e5bff2b430b3fe4437910e262ad7bdee
futeng <ifuteng@gmail.com>
2013-05-20 00:03:18 +0800

    first create

---
+++ stringparser/src/main/java/org/futeng/stringparser/App.java
@@ -0,0 +1,13 @@
+package org.futeng.stringparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
